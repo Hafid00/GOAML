92680aeaec4079afd551dd84be12821f3cad4eef
Hafid Chaikh Sidi Boubakkar <hafid.chaikhsidiboubakkar@gmail.com>
2019-05-17 11:42:24 +0100

    yy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,37 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 
 
+
 export default function Navbar() {
-  return (
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          GoAML
-        </a>
-        <form className="navbar-form navbar-left" action="/">
-        <Link to="/" className="btn btn-danger">
-        <i class="fas fa-sign-out-alt">LogOut</i>
-        </Link>   
-    </form>
-       
-      
-      </div>
-    </nav>
-  );
+
+    return (
+
+        <nav className="navbar navbar-dark bg-dark ">
+
+            <div className="container-fluid">
+
+                <a className="navbar-brand  mx-auto font-weight-bold" href="/" >GoAML</a>
+
+                <form className="navbar-form navbar-left" action="/">
+
+                    <Link to="/"  className="btn btn-outline-secondary ">
+
+                        <i className="fas fa-sign-out-alt" >  LogOut  </i>
+
+                    </Link >
+
+                </form>
+
+
+
+
+
+            </div>
+
+        </nav>
+
+    );
+
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,35 +1,109 @@
 import React, { Component } from 'react'
+
 import { withRouter,Link } from 'react-router-dom';
 
 
-class home extends Component {
-  
-  render() {
-    return (
 
-    
-      <div className="home" class="container">
-     
-                <div>
-                  <div class="homeButton">
-                         <Link to="/strPhysique" className="btn btn-primary mb-3">STRPhysique</Link>                     
-                          <Link to="/strMorale" className="btn btn-primary mb-3">STRMorale</Link>
+class home extends Component {
+
+
+
+    render() {
+
+        return (
+
+
+
+
+
+            <div  className="container">
+
+                <div className="row my-5">
+
+                    <div className="col-md-4 my-5 mx-auto">
+
+                        <div className="card bg-transparent" >
+
+                            <div className="card-header text-center font-weight-bold">Déclaration STR</div>
+
+                            <ul className="list-group list-group-flush">
+
+                                <Link to="/strPhysique" className="list-group-item btn btn-outline-primary font-weight-bold">STR Physique</Link>
+
+                                <Link to="/strMorale" className="list-group-item btn btn-outline-primary font-weight-bold">STR Morale</Link>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                    <div className="col-md-4 my-5 mx-auto">
+
+                        <div className="card bg-transparent my-5 mx-auto" >
+
+                            <div className="card-header text-center font-weight-bold">Consultation</div>
+
+                            <ul className="list-group list-group-flush">
+
+                                <Link to="/consultation" className="list-group-item btn btn-outline-primary font-weight-bold">Consulter</Link>
+
+
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                    <div className="col-md-4 my-5 mx-auto">
+
+                        <div className="card bg-transparent" >
+
+                            <div className="card-header text-center font-weight-bold">Déclaration AIF</div>
+
+                            <ul className="list-group list-group-flush">
+
+                                <Link to="/aifPhysique" className="list-group-item btn btn-outline-primary font-weight-bold">AIF Physique</Link>
+
+                                <Link to="/aifMorale" className="list-group-item btn btn-outline-primary font-weight-bold">AIF Morale</Link>
+
+
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
                 </div>
-                <div class="homeButton">
-                      <Link to="/aifPhysique" className="btn btn-primary mb-3">AIFPhysique</Link>   
-                      <Link to="/aifMorale" className="btn btn-primary mb-3">AIFMorale</Link>   
-                </div>
-                <div class="homeButtonLast"> 
-                <Link to="/addProjectTask" className="btn btn-primary mb-3">Consultation
-                <i className="far fa-sign-out">LogOut</i></Link>                </div>
-                </div>
-      </div> 
-    )
-  }
+
+
+
+
+
+            </div>
+
+        )
+
+    }
+
 }
+
 export default withRouter(home);
+
 // function strPhysique() {
+
 //   return (
+
 //     <strPhysique/>
+
 //   );
+
 // }
